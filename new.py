0ef72e89c66dc92cfd9ee6fa3d261634d75eee4f
AhmedBakrElsaid <a.bakr2472001@gmail.com>
2025-10-27 18:04:12 +0300

    password verifications

---
+++ new.py
@@ -0,0 +1,29 @@
+import string
+import random
+
+s1 = list(string.ascii_lowercase)
+s2 = list(string.ascii_uppercase)
+s3 = list(string.digits)
+s4 = list(string.punctuation)
+
+characters_numbers = input("how many characters for the password? ")
+
+while True:
+    try:
+        characters_numbers = int(characters_numbers)
+        if characters_numbers < 6:
+            print("you need at least 6 characters.")
+            characters_numbers = input ("please enter the number again: ")
+        else:
+            break
+    except:
+        print("please Enters numbers only")
+        characters_numbers = input("how many characters for the password? ")
+
+
+
+random.shuffle(s1)
+random.shuffle(s2)
+random.shuffle(s3)
+random.shuffle(s4)
+
